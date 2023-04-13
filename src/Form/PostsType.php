c02f64e1f023125eedabe90ca73087b26cb9517c
CSI <CSI@campus.local>
2023-04-13 16:59:15 +0200

    form posts

--- src/Form/PostsType.php
+++ src/Form/PostsType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Posts;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +13,14 @@ class PostsType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
+            ->add('title',TextType::class,
+            [
+                'required'=>'true',
+                'attr'=>['class'=>'']
+            ])
             ->add('likes')
-            ->add('type')
+            ->add('type', TextType::class, ['attr'=>["class"=>"border-none text-red-400 focus:outline-none focus:border-none px-1 outline-none bg-emerald-700"],'label' => 'type de post','label_attr'=>["class"=>"border-none text-white bg-emerald-700"]]
+            )
             /* ->add('user_id') */
         ;
     }
